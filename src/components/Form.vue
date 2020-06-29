ac20e34a165e1ba220ffafd4a0e8990011da6af3
dalai <dalai2123@hotmail.com>
2020-06-29 10:35:34 -0500

    remade form, solved overflow

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,90 +1,43 @@
 <template>
-  <b-container d-flex justify-content-center>
-    <b-form  @submit="onSubmit" @reset="onReset" v-if="show" method="POST" data-netlify="true"
-    name='form-contact'>
+  <b-container class="cont">
+<form name="contact-me" netlify netlify-honeypot="bot-field" class="form-group" >
+  <p>{{ $t('contact.name') }}</p>
+  <input class="inputs" :placeholder="$t('contact.name')" type="text" name="name" />
+    <p>{{ $t('contact.email') }}</p>
+  <input class="inputs" :placeholder="$t('contact.email')" type="text" name="email" />
+    <p>{{ $t('contact.message') }}</p>
+  <textarea class="inputs" :placeholder="$t('contact.message')" name="question"></textarea>
 
 
-      <b-form-group id="input-group-1" :label="$t('contact.name')" label-for="input-1">
-        <b-form-input
-          name="name"
-          id="input-1"
-          v-model="form.name"
-          required
-          :placeholder="$t('contact.name')"
-        ></b-form-input>
-      </b-form-group>
-            <b-form-group
-        class="form-group"  
-        id="input-group-2"
-        :label=" $t('contact.email')  "
-        label-for="input-2"
-        required
-      >
-        <b-form-input
-        name="email"
-          id="input-1"
-          v-model="form.email"
-          type="email"
-          required
-          :placeholder=" $t('contact.email')"
-        ></b-form-input>
-      </b-form-group>
-      <b-form-group :label="$t('contact.message')" >
-          <b-form-textarea
-          name="message"
-    id="textarea-no-resize"
-    :placeholder="$t('contact.title')"
-    rows="3"
-    no-resize
-  ></b-form-textarea>
-      </b-form-group >
       <div class="field">
         <div data-netlify-recaptcha="true"></div>
       </div>
       <div class="bord">
-<button class="btn btn--animation-from-right">
+<button class="btn btn--animation-from-right" type="submit">
   <span class="btn__text-static"> {{ $t('contact.send') }}</span>
   <div class="btn__text-dynamic">
     <span class="btn__text-dynamic-inner">{{ $t('contact.send') }}</span>
   </div>
 </button>
 </div>
-    </b-form>
+</form>
   </b-container>
 </template>
 
 <script scoped>
   export default {
-    data() {
-      return {
-        form: {
-          email: '',
-          name: '',
-        },
-        show: true,
-      }
-    },
-    methods: {
-      onSubmit(evt) {
-        evt.preventDefault()
-        alert(JSON.stringify(this.form))
-      },
-      onReset(evt) {
-        evt.preventDefault()
-        // Reset our form values
-        this.form.email = ''
-        this.form.name = ''
-        // Trick to reset/clear native browser form validation state
-        this.show = false
-        this.$nextTick(() => {
-          this.show = true
-        })
-      }
-    }
+    components:'Form'
   }
 </script>
 <style lang="scss" scoped>
 @import "../styles/global.scss";
+
+.inputs{
+  width: 70vw;
+  padding: 10px;
+  margin:0 auto;
+  display: block;
+}
 .form-group{
   color:#fff;
 }
